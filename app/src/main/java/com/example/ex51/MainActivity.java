a30351140030c82d824cc5bda5d96feb0c416b76
nzo2020 <nz2020@bs.amalnet.k12.il>
2023-12-30 22:59:08 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ex51/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.ex51;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+
+    TextView tView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tView = findViewById(R.id.tView);
+        btn = findViewById(R.id.btn);
+    }
+
+
+    public void clicked(View view) {
+        tView.setText("Oh, yea, I’ve been clicked!");
+    }
+}
